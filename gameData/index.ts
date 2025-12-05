0c178b36ee79208829a58a6ab3db070dfdc4c50c
Bee / Simon <stockfisch97@gmail.com>
2025-12-05 19:03:28 +0100

    Update index.ts

--- gameData/index.ts
+++ gameData/index.ts
@@ -1,24 +1,24 @@
-import { CategoryConfig, ResourceConfig, TaskConfig, ActionConfig, ItemConfig, SlotConfig } from "../types";
+import { CategoryConfig, ResourceConfig, TaskConfig, ActionConfig, ItemConfig, SlotConfig, ConverterConfig } from "../types";
 
 import * as CategoriesModule from './categories';
 import * as ResourcesModule from './resources';
 import * as TasksModule from './tasks';
 import * as ActionsModule from './actions';
 import * as EquipmentModule from './equipment';
-import * as ChristmasModule from './christmas';
-import * as QuestCatModule from './questlines/quest_cat';
-import * as QuestMirrorModule from './questlines/quest_mirror';
+import * as ConvertersModule from './converters';
+
 
 const modules: any[] = [
     CategoriesModule,
     ResourcesModule,
     TasksModule,
     ActionsModule,
-    EquipmentModule
+    EquipmentModule,
+    ConvertersModule
 ];
 
 // import and add quest modules
-modules.push(QuestCatModule, QuestMirrorModule);
+//modules.push(QuestModule1);
 
 
 const allCategories: CategoryConfig[] = [];
@@ -27,10 +27,9 @@ const allTasks: TaskConfig[] = [];
 const allActions: ActionConfig[] = [];
 const allItems: ItemConfig[] = [];
 const allSlots: SlotConfig[] = [];
+const allConverters: ConverterConfig[] = [];
 const now = new Date();
-if (now.getMonth() === 11) { // December is month 11 (0-indexed)    
-    modules.push(ChristmasModule);
-}
+
 
 modules.forEach(mod => {
     if (mod.CATEGORIES) allCategories.push(...mod.CATEGORIES);
@@ -39,6 +38,7 @@ modules.forEach(mod => {
     if (mod.ACTIONS) allActions.push(...mod.ACTIONS);
     if (mod.ITEMS) allItems.push(...mod.ITEMS);
     if (mod.SLOTS) allSlots.push(...mod.SLOTS);
+    if (mod.CONVERTERS) allConverters.push(...mod.CONVERTERS);
 });
 
 export const CATEGORIES = allCategories;
@@ -47,3 +47,4 @@ export const TASKS = allTasks;
 export const ACTIONS = allActions;
 export const ITEMS = allItems;
 export const SLOTS = allSlots;
+export const CONVERTERS = allConverters;
